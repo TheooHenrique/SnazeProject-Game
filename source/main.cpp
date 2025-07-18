9a6a9464650a2f68fa31abb4f0b28c487950b54a
honey <105451372+haninfoif@users.noreply.github.com>
2025-07-17 23:43:11 -0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -5,6 +5,12 @@
 
 #include "simulation.hpp"
 
+/**
+ * @brief The main function that drives the game.
+ * @param argc The number of command-line arguments.
+ * @param argv An array of command-line argument strings.
+ * @return 0 on successful execution, non-zero otherwise.
+ */
 int main(int argc, char* argv[]) {
   // SnazeSimulation is a singleton.
   SnazeSimulation &sg = SnazeSimulation::get_instance(); //Initalize Singleton sg (snakegame)
@@ -12,9 +18,9 @@ int main(int argc, char* argv[]) {
 
   // The Game Loop.
   while (not sg.is_over) {
-    sg.process_events();
-    sg.update();
-    sg.render();
+    sg.process_events(); // Handle current state logic.
+    sg.update();         // Transition to the next state.
+    sg.render();         // Render the current game screen.
   }
   return 0;
 }
