bee0300aa99e3dcc78f5c4882924be9ce9c40256
honey <105451372+haninfoif@users.noreply.github.com>
2025-07-17 23:44:05 -0300

    Update positiondirection.cpp

--- source/positiondirection.cpp
+++ source/positiondirection.cpp
@@ -1,17 +1,17 @@
 #include <cstddef>
 #include "positiondirection.hpp"
+
+/**
+ * @file positiondirection.cpp
+ * @brief Implements the simple getters and setters for Position and Direction.
+ */
+
 //====================Position Func========================
-//Getters
 size_t Position::get_x(){return this->m_x;}
 size_t Position::get_y(){return this->m_y;}
-
-//Setters
 void Position::set_y(size_t y){ this->m_y = y;}
 void Position::set_x(size_t x){ this->m_x = x;}
 
 //====================Direction Func========================
-//Setters
 void Direction::set_dir(size_t dir){this->m_current_dir = dir;}
-
-//Getters
 size_t Direction::get_dir(){return this->m_current_dir;}
