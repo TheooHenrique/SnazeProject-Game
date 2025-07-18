75c3fbcc801848629ddecece294fe6c9742de734
honey <105451372+haninfoif@users.noreply.github.com>
2025-07-17 23:44:58 -0300

    Update reader.hpp

--- source/reader.hpp
+++ source/reader.hpp
@@ -1,19 +1,43 @@
 #ifndef READER
 #define READER
+
 #include "level.hpp"
 #include <deque>
 #include <fstream>
 #include <string>
+
+/**
+ * @file reader.hpp
+ * @brief Defines the Reader class for parsing level configuration files.
+ */
+
+/**
+ * @class Reader
+ * @brief A utility class to read and parse game level data from a file.
+ */
 class Reader{
     private:
-    std::string m_filename;
+        /// @brief The filename of the input file (not currently used).
+        std::string m_filename;
 
     public:
-    Reader(){} //Constructor
+        /**
+         * @brief Default constructor.
+         */
+        Reader(){}
 
-    void trim(std::string&);
-    std::deque<Level> parse_cfg(std::ifstream&);
+        /**
+         * @brief Trims leading and trailing whitespace from a string.
+         * @param s The string to be trimmed in-place.
+         */
+        void trim(std::string& s);
+
+        /**
+         * @brief Parses an input file stream containing one or more levels.
+         * @param file The input file stream to read from.
+         * @return A deque of Level objects parsed from the file.
+         */
+        std::deque<Level> parse_cfg(std::ifstream& file);
 };
-
 
 #endif
